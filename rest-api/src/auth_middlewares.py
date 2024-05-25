4f04255899f070054fe6c74b14d0c81a61217303
crucials <ensueness@gmail.com>
2024-05-25 13:47:58 +0500

    added doc comment to `authorize_request` middleware

--- rest-api/src/auth_middlewares.py
+++ rest-api/src/auth_middlewares.py
@@ -6,9 +6,16 @@ from werkzeug.exceptions import Unauthorized
 
 
 def authorize_request():
+    """
+    this middleware registered on all routes by default
+
+    gets account data from jwt in `Authorization` header and puts in
+    `Account` header as json. if token is invalid, does nothing
+    """
     updated_headers = Headers(flask.request.headers)
     updated_headers.add('Account', json.dumps({'name': 'u', 'password': '1236'}))
     flask.request.headers = updated_headers
+
 
 def restrict_unauthorized_access():
     if flask.request.headers.get('Account') is None:
