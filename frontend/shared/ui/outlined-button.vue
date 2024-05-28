2ccdebe45164f0dce26feaea92e200c0e4bf6063
crucials <ensueness@gmail.com>
2024-05-28 20:23:21 +0500

    added lighting shadow on hover for outlined button

--- frontend/shared/ui/outlined-button.vue
+++ frontend/shared/ui/outlined-button.vue
@@ -4,7 +4,8 @@
     >
         <div
             class="py-3 px-8 bg-neutral-900 rounded-md text-base xs:text-sm
-                transition-colors duration-300 hover:bg-neutral-800"
+                transition-all duration-300 hover:bg-neutral-800
+                hover:shadow-xl hover:shadow-purple-400/10"
         >
             <slot></slot>
         </div>
