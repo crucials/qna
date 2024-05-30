abb0e1cd41496d34785638f0139c0bcf446ef271
crucials <ensueness@gmail.com>
2024-05-30 08:40:22 +0500

    moved flask app creation in a separate module

--- rest-api/src/main.py
+++ rest-api/src/main.py
@@ -1,54 +1,14 @@
 import os
 
 from dotenv import load_dotenv
-from flask_cors import CORS
-from marshmallow import ValidationError
 import waitress
-from flask import Flask
-from werkzeug.exceptions import HTTPException
 
-from auth_middlewares import authorize_request
-from controllers import controllers_blueprints
+from flask_app import create_flask_app
 
 
 load_dotenv()
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-cors = CORS(app, origins=os.environ.get('FRONTEND_ORIGIN'))
-
-for blueprint in controllers_blueprints:
-    app.register_blueprint(blueprint)
-
-
-@app.errorhandler(HTTPException)
-def send_json_error_response(error: HTTPException):
-    return {
-        'error': {
-            'code': error.code,
-            'explanation': error.description
-        },
-        'data': None,
-    }, error.code or 500
-
-
-@app.errorhandler(ValidationError)
-def send_json_validation_error_response(error: ValidationError):
-    invalid_fields = list(error.messages_dict)
-    message = (error.messages_dict.get(invalid_fields[0])
-                or 'invalid request body')
-    return {
-        'error': {
-            'code': 400,
-            'field': invalid_fields[0],
-            'explanation': message[0],
-        },
-        'data': None,
-    }, 400
-
-
-app.before_request(authorize_request)
+app = create_flask_app()
 
 if os.environ.get('MODE') == 'DEV':
     app.run(port=8000)
