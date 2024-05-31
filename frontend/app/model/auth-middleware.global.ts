a86a8844f83b39abae8febd1b4fd5ecaf46dbf70
crucials <ensueness@gmail.com>
2024-05-31 07:18:07 +0500

    optimized auth middleware

--- frontend/app/model/auth-middleware.global.ts
+++ frontend/app/model/auth-middleware.global.ts
@@ -1,7 +1,12 @@
 import { getCurrentAccount } from '~/app/api/get-current-account'
 import { useCurrentAccountStore } from '~/shared/model/current-account-store'
+import { useTokenCookie } from '~/shared/model/token-cookie'
 
 export default defineNuxtRouteMiddleware(async () => {
+    const token = useTokenCookie()
     const { account } = storeToRefs(useCurrentAccountStore())
-    account.value = await getCurrentAccount()
+
+    if(!account.value && token.value) {
+        account.value = await getCurrentAccount()
+    }
 })
