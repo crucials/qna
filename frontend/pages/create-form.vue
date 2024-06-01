365117aa417ca9ff80b1bbe905cb8b64eacaea92
crucials <ensueness@gmail.com>
2024-06-01 17:06:53 +0500

    added form creation page

---
+++ frontend/pages/create-form.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import authorizedOnlyMiddleware from '~/shared/model/authorized-only-middleware'
+
+definePageMeta({
+    path: '/forms/create',
+    middleware: authorizedOnlyMiddleware
+})
+</script>
+
+<template>
+    
+</template>
