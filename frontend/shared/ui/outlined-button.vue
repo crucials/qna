5e0b2d9fa3c5fecb068fc14fb76b8be41fbba62f
crucials <ensueness@gmail.com>
2024-06-02 14:17:22 +0500

    added flexbox layout to buttons

--- frontend/shared/ui/outlined-button.vue
+++ frontend/shared/ui/outlined-button.vue
@@ -11,7 +11,8 @@ withDefaults(defineProps<{
         :type="tag === 'button' ? 'button' : undefined"
     >
         <div
-            class="py-2.5 px-8 bg-neutral-900 rounded-md text-base xs:text-sm
+            class="flex items-center gap-x-3
+                py-2.5 px-7 bg-neutral-900 rounded-md text-base xs:text-sm
                 transition-all duration-300 hover:bg-neutral-800
                 hover:shadow-xl hover:shadow-purple-400/20"
         >
--- frontend/shared/ui/solid-button.vue
+++ frontend/shared/ui/solid-button.vue
@@ -7,7 +7,8 @@ withDefaults(defineProps<{
 <template>
     <component
         :is=tag
-        class="block py-2.5 px-8 bg-amethyst rounded-md text-base xs:text-sm
+        class="flex items-center gap-x-3
+            py-2.5 px-8 bg-amethyst rounded-md text-base xs:text-sm
             transition-colors duration-300 hover:bg-amethyst-dark"
         :type="tag === 'button' ? 'button' : undefined"
     >
