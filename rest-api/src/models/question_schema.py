9d04694bdaaafea28eec0623263e89247c56fd3c
crucials <ensueness@gmail.com>
2024-06-03 07:17:41 +0500

    replaced enum with one-of validation rule

--- rest-api/src/models/question_schema.py
+++ rest-api/src/models/question_schema.py
@@ -1,26 +1,24 @@
 from enum import Enum
-from typing import TypedDict
+from typing import Literal, TypedDict, get_args
 
 from marshmallow import Schema, fields, validate
 
 
-class QuestionType(Enum):
-    SHORT_TEXT = 'SHORT_TEXT'
-    MULTILINE_TEXT = 'MULTILINE_TEXT'
-    ONE_OPTION = 'ONE_OPTION'
+QuestionType = Literal['SHORT_TEXT', 'MULTILINE_TEXT', 'ONE_OPTION', 'MULTIPLE_OPTION']
+QUESTION_TYPES = get_args(QuestionType)
 
 
 class Question(TypedDict):
     text: str
     optional: bool
-    type: QuestionType
+    type: str
     options: list[str]
 
 
 class QuestionValidationSchema(Schema):
     text = fields.Str(required=True, validate=validate.Length(min=3, max=300))
     optional = fields.Bool(required=True)
-    type = fields.Enum(QuestionType, required=True)
+    type = fields.Str(required=True, validate=validate.OneOf(QUESTION_TYPES))
 
     options = fields.List(
         fields.Str(validate=validate.Length(min=1, max=150))
--- rest-api/src/services/surveys.py
+++ rest-api/src/services/surveys.py
@@ -11,11 +11,8 @@ class SurveysService:
 
         for question in questions:
             question['survey_id'] = survey['_id']
-            question['type'] = question['type'].__str__()
 
-        questions_insert_result = questions_collection.insert_many(questions)
-
-        survey['questions_ids'] = questions_insert_result.inserted_ids
+        questions_collection.insert_many(questions)
 
         return accounts_collection.find_one_and_update({
             '_id': account_id
