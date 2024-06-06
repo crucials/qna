97abedf88bd177422d7999f6307f6a6386e79d23
crucials <ensueness@gmail.com>
2024-06-06 07:07:44 +0500

    decreased padding on mobile screens

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col min-h-screen overflow-x-hidden">
         <Navigation />
-        <main class="flex-grow px-16 py-12 lg:py-9 lg:px-12 sm:p-7">
+        <main class="flex-grow px-16 py-12 lg:py-9 lg:px-12 sm:p-7 xs:p-5">
             <NuxtPage />
         </main>
 
