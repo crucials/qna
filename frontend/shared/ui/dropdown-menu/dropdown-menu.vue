1a34215938a1624b832a280bd1a41e1c8a807568
crucials <ensueness@gmail.com>
2024-06-12 09:51:21 +0500

    fixed z-index and url overflowing issues

--- frontend/shared/ui/dropdown-menu/dropdown-menu.vue
+++ frontend/shared/ui/dropdown-menu/dropdown-menu.vue
@@ -22,7 +22,7 @@ onClickOutside(dropdownWrapper, () => opened.value = false)
                 role="listbox"
                 id="dropdownList"
                 class="transition-all duration-300
-                    absolute top-[calc(100%+10px)] right-0
+                    absolute top-[calc(100%+10px)] right-0 z-20
                     flex flex-col gap-y-3
                     p-4 rounded-md bg-black/80 border border-neutral-600"
                 :class="dropdownListClass"
