0f0a1abd6d7b8336de5eb317b51dabc4ef8707d9
crucials <ensueness@gmail.com>
2025-01-28 09:27:15 +0500

    google auth: implement token cookie update on callback, add a loading indicator

--- frontend/pages/google-auth-callback/ui/google-auth-callback.vue
+++ frontend/pages/google-auth-callback/ui/google-auth-callback.vue
@@ -1,14 +1,35 @@
 <script setup lang="ts">
+import { useTokenCookie } from '~/shared/model/token-cookie'
+
 definePageMeta({
     path: '/auth-callback',
 })
 
 const route = useRoute()
-const googleAuthData = new URLSearchParams(route.hash)
+const router = useRouter()
+
+const token = useTokenCookie()
+
+onMounted(() => {
+    const googleAuthServerResponseData = new URLSearchParams(route.hash)
+    const authError = googleAuthServerResponseData.get('error')
+    const idToken = googleAuthServerResponseData.get('id_token')
+
+    if (authError !== null || idToken === null) {
+        console.error('Google OAuth error' + authError ? `: ${authError}` : '')
+
+        throw createError('Log in failed')
+    }
+
+    token.value = idToken
+    router.push('/dashboard')
+})
 </script>
 
 <template>
-    {{ googleAuthData.entries().toArray() }}
+    <div>
+        <Spinner class="w-20 h-20 mx-auto my-20" />
+    </div>
 </template>
 
 <style scoped></style>
---
+++ frontend/shared/ui/spinner.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div
+        class="w-16 h-16 border-4 rounded-full border-neutral-700 border-r-white animate-spin"
+    ></div>
+</template>
