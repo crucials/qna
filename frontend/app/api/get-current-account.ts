cfb9aece7f4e3ebef224b4c492aa4ed517b574fb
crucials <ensueness@gmail.com>
2025-02-01 15:06:36 +0500

    frontend/auth: type `getCurrentAccount` function return value

--- frontend/app/api/get-current-account.ts
+++ frontend/app/api/get-current-account.ts
@@ -1,6 +1,7 @@
 import { fetchApi } from '~/shared/api/fetch-api'
+import type { Account } from '~/shared/model/current-account-store'
 
 export async function getCurrentAccount() {
-    const response = await fetchApi('/current-account')
-    return response.data.value?.data || null
+    const response = await fetchApi<Account>('/current-account')
+    return response.data.value
 }
