269555f1c878baf88f6268d06d700cfa05143f8b
crucials <ensueness@gmail.com>
2025-02-02 08:44:56 +0500

    frontend/auth: cache access token payload to decode it only when the token changes

--- frontend/shared/model/token-store.ts
+++ frontend/shared/model/token-store.ts
@@ -5,23 +5,33 @@ export const useAccessTokenStore = defineStore('access-token', () => {
         maxAge: 2592000,
     })
 
-    function checkIfAccessTokenExpired() {
+    const accessTokenExpirationTimestamp = computed(() => {
         if (!accessToken.value) {
-            return false
+            return undefined
         }
 
         const encodedPayload = accessToken.value.split('.')[1]
 
         if (encodedPayload === undefined) {
-            return false
+            return undefined
         }
 
         const unverifiedPayload = JSON.parse(
             Buffer.from(encodedPayload, 'base64').toString(),
         )
 
-        return unverifiedPayload.exp * 1000 <= new Date().getTime()
+        return unverifiedPayload.exp * 1000
+    })
+
+    function checkIfAccessTokenExpired() {
+        if (accessTokenExpirationTimestamp.value === undefined) {
+            return false
+        }
+
+        return (
+            accessTokenExpirationTimestamp.value <= new Date().getTime()
+        )
     }
 
-    return { accessToken, checkIfAccessTokenExpired }
+    return { accessToken, checkIfAccessTokenExpired, accessTokenExpirationTimestamp }
 })
