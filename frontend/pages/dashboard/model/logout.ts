9cb2f735231f495aaab8c57b36b7753d4e3b2a03
crucials <ensueness@gmail.com>
2025-02-02 08:59:33 +0500

    frontend/auth: call `/auth/logout` endpoint on logout

--- frontend/pages/dashboard/model/logout.ts
+++ frontend/pages/dashboard/model/logout.ts
@@ -1,12 +1,16 @@
+import { sendLogoutRequest } from '~/pages/dashboard/api/logout'
 import { useCurrentAccountStore } from '~/shared/model/current-account-store'
 import { useAccessTokenStore } from '~/shared/model/token-store'
 
-export function logout() {
+export async function logout() {
     const { accessToken } = storeToRefs(useAccessTokenStore())
     const { account } = storeToRefs(useCurrentAccountStore())
     const router = useRouter()
 
     accessToken.value = null
     account.value = null
+
+    await sendLogoutRequest()
+
     router.push('/')
 }
