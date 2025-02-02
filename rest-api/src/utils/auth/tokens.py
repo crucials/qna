7dc024730ca221cbf685f193aa1b865a26a1b190
crucials <ensueness@gmail.com>
2025-02-02 08:21:29 +0500

    backend/auth: set access token lifetime to 15 minutes

--- rest-api/src/utils/auth/tokens.py
+++ rest-api/src/utils/auth/tokens.py
@@ -7,7 +7,7 @@ import jwt
 _ACCESS_JWT_SECRET_KEY = os.environ["ACCESS_JWT_SECRET_KEY"]
 _REFRESH_JWT_SECRET_KEY = os.environ["REFRESH_JWT_SECRET_KEY"]
 
-_ACCESS_TOKEN_LIFETIME_MINUTES = 20
+_ACCESS_TOKEN_LIFETIME_MINUTES = 15
 _REFRESH_TOKEN_LIFETIME_DAYS = 30
 
 _JWT_ALGORITHM = "HS256"
@@ -15,7 +15,7 @@ _JWT_ALGORITHM = "HS256"
 
 def create_access_token(payload: dict):
     expiration_time = datetime.datetime.now() + datetime.timedelta(
-        seconds=_ACCESS_TOKEN_LIFETIME_MINUTES,
+        minutes=_ACCESS_TOKEN_LIFETIME_MINUTES,
     )
 
     return jwt.encode(
